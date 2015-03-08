7d5f6536b0a083da9f9c42b311e1ee76d09ab984
wreckday <yang23567@hotmail.com>
2015-03-08 10:06:36 -0400

    3/7

--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -1,7 +1,8 @@
 /**
  * Created by Mellon on 3/8/15.
 
- Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+ Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ compute how much water it is able to trap after raining.
 
  For example,
  Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
